13241a546373583d742d203739681ecfb389ccf9
m-arevalo-ez <128000050+m-arevalo-ez@users.noreply.github.com>
2023-09-04 23:33:13 -0300

    add test calculate word value

--- tests/test_calculate_word_value.py
+++ tests/test_calculate_word_value.py
@@ -29,5 +29,70 @@ class TestCalculateWordValue(unittest.TestCase):
         value = calcular_valor_palabra(word)
         self.assertEqual(value, 7)
 
+    def test_with_letter_multiplier(self):
+        word = [
+            Cell(letter=Tile('C', 1)),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='letter',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calcular_valor_palabra(word)
+        self.assertEqual(value, 7)
+
+    def test_with_word_multiplier(self):
+        word = [
+            Cell(letter=Tile('C', 1)),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='word',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calcular_valor_palabra(word)
+        self.assertEqual(value, 10)
+
+    def test_with_letter_word_multiplier(self):
+        word = [
+            Cell(
+                multiplier=3,
+                multiplier_type='letter',
+                letter=Tile('C', 1)
+            ),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='word',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calcular_valor_palabra(word)
+        self.assertEqual(value, 14)
+
+    def test_with_letter_word_multiplier_no_active(self):
+        # QUE HACEMOS CON EL ACTIVE ????
+        word = [
+            Cell(
+                multiplier=3,
+                multiplier_type='letter',
+                letter=Tile('C', 1)
+            ),
+            Cell(letter=Tile('A', 1)),
+            Cell(
+                letter=Tile('S', 2),
+                multiplier=2,
+                multiplier_type='word',
+            ),
+            Cell(letter=Tile('A', 1)),
+        ]
+        value = calcular_valor_palabra(word)
+        self.assertEqual(value, 5)    
+
 if __name__ == '__main__':
     unittest.main()
